31297091888fc3757fc992c5d8de464719a02d18
Osaetin Evbuoma <osaetinevbuoma@gmail.com>
2022-04-01 14:15:25 +0100

    Updated README file

--- crawler/parser.py
+++ crawler/parser.py
@@ -7,8 +7,8 @@ from crawler.custom_exceptions import UrlNotFoundException
 class Parser:
     """
     Although we are using BeautifulSoup library to parse HTML content, the purpose of
-    the Parser wrapper class is to wrap the functionality our selected parsing library. This way, if
-    in the future there is the need to use a different parsing library, that switch can be done
+    the Parser wrapper class is to wrap the functionality of our selected parsing library. This way,
+    if in the future there is the need to use a different parsing library, that switch can be done
     very easily in the Parser class without affecting the rest of the application.
     """
 
